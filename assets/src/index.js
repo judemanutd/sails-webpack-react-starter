7ff32806304df6c84da0f880092e1bf819073f02
Jude <jude.manutd@gmail.com>
2018-07-19 22:16:01 +0530

    initial commit

---
+++ assets/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return <div>Sails React Webpack Starter </div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
